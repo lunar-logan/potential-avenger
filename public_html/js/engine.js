a9d7d33f8746028253350d6764ecf2051862ee5d
dell <dell@com>
2014-04-23 18:34:02 +0530

    initial commit

---
+++ public_html/js/engine.js
@@ -0,0 +1,25 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+function Engine(gameLoop, refreshTime) {
+    this.refreshTime = 50; // 50ms by default
+    if (refreshTime !== undefined)
+        this.refreshTime = refreshTime;
+
+    this.gameLoop = null;
+    if (gameLoop !== undefined)
+        this.gameLoop = gameLoop;
+
+    this.react = function() {
+        console.log("MayaFx engine is begining to react");
+        if (this.gameLoop !== null) {
+            window.setInterval(this.gameLoop, this.refreshTime);
+        } else {
+            console.log("Cannot react, gameLoop is null");
+        }
+    };
+}
